6310dd98a2901729aea0037e766a11639b250514
madrackwp <52032916+madrackwp@users.noreply.github.com>
2023-03-14 15:05:54 +0800

    Found a library that is able to detect voices well

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const startBtn = document.querySelector("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const result = document.querySelector("#display");
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var recognition = new SpeechRecognition();
+
+// console.log(recognition);
 
 var colors = [
   "aqua",
@@ -52,59 +59,33 @@ var colors = [
   "yellow",
 ];
 
-var recognition = new SpeechRecognition();
-if (SpeechGrammarList) {
-  // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
-  // This code is provided as a demonstration of possible capability. You may choose not to use it.
-  var speechRecognitionList = new SpeechGrammarList();
-  var grammar = "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
-  speechRecognitionList.addFromString(grammar, 1);
-  recognition.grammars = speechRecognitionList;
-}
-recognition.continuous = false;
+var speechRecognitionList = new SpeechGrammarList();
+var grammar = "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = true;
 recognition.lang = "en-US";
-recognition.interimResults = false;
+recognition.interimResults = true;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector(".output");
-var bg = document.querySelector("html");
-var hints = document.querySelector(".hints");
-
-var colorHTML = "";
-colors.forEach(function (v, i, a) {
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + " </span>";
-});
-hints.innerHTML = "Tap/click then say a color to change the background color of the app. Try " + colorHTML + ".";
-
-document.body.onclick = function () {
+startBtn.addEventListener("click", () => {
   recognition.start();
-  console.log("Ready to receive a color command.");
-};
+  console.log("LISTENING");
+});
 
-recognition.onresult = function (event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The first [0] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The second [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
-  var color = event.results[0][0].transcript;
-  diagnostic.textContent = "Result received: " + color + ".";
-  bg.style.backgroundColor = color;
-  console.log("Confidence: " + event.results[0][0].confidence);
-};
-
-recognition.onspeechend = function () {
+stopBtn.addEventListener("click", () => {
+  console.log("CONTINUOUS LISTENING STOPPED");
   recognition.stop();
+});
+recognition.onresult = (event) => {
+  console.log(event.results);
+  var noOfPhrases = event.results.length;
+  // console.log(noOfPhrases);
+  var lastSentence = event.results[noOfPhrases - 1][0].transcript;
+  result.innerHTML = result.innerHTML + " " + lastSentence;
 };
 
-recognition.onnomatch = function (event) {
-  diagnostic.textContent = "I didn't recognise that color.";
-};
-
-recognition.onerror = function (event) {
-  diagnostic.textContent = "Error occurred in recognition: " + event.error;
+recognition.onspeechend = () => {
+  console.log("STOPPED LISTENING");
+  recognition.stop();
 };
